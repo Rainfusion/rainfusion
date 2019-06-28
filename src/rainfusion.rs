2df486a9450e6ad4eae1b0f3bdba107c2236b417
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2019-06-28 14:18:26 +0100

    Replace dotenv with env variables

--- src/rainfusion.rs
+++ src/rainfusion.rs
@@ -1,8 +1,8 @@
 //! This file provides functions to fetch data from the main Rainfusion
 //! CDN provided by the .env file in the root directory of this project.
-use dotenv_codegen::dotenv;
 use futures::{future, Future};
 use js_sys::{Error, Promise};
+use std::env;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::{future_to_promise, JsFuture};
 use web_sys::{Document, Request, RequestInit, RequestMode, Response, Window};
@@ -29,6 +29,9 @@ impl Rainfusion {
         ok_callback: Closure<FnMut(JsValue)>,
         err_callback: Closure<FnMut(JsValue)>,
     ) -> Result<(), JsValue> {
+        // Assert if variables are not in the environment.
+        assert!(env::var("CDN_IP").is_err());
+
         // Construct Request Options.
         let mut opts = RequestInit::new();
         opts.method("GET");
@@ -36,7 +39,7 @@ impl Rainfusion {
 
         // Construct Request.
         let request =
-            Request::new_with_str_and_init(&format!("{}/api/mods", dotenv!("CDN_IP")), &opts)?;
+            Request::new_with_str_and_init(&format!("{}/api/mods", env::var("CDN_IP").unwrap()), &opts)?;
 
         // Fetch using the request.
         let request_promise = self.window.fetch_with_request(&request);
@@ -73,29 +76,38 @@ impl Rainfusion {
 
     /// Modify the Launcher
     pub fn rainfusion_launcher(&self) -> Result<(), JsValue> {
+        // Assert if variables are not in the environment.
+        assert!(env::var("CDN_IP").is_err());
+        assert!(env::var("SUBMIT_URL").is_err());
+        assert!(env::var("DOCS_URL").is_err());
+        assert!(env::var("UPDATE_URL").is_err());
+        assert!(env::var("DISCORD_URL").is_err());
+
         // Get Download Icon
         let launcher_download = self
             .document
             .get_element_by_id("launcher-download")
             .unwrap();
-        launcher_download
-            .set_attribute("href", &format!("{}/launcher-download", dotenv!("CDN_IP")))?;
+        launcher_download.set_attribute(
+            "href",
+            &format!("{}/launcher-download", &env::var("CDN_IP").unwrap()),
+        )?;
 
         // Get Submit Icon
         let launcher_submit = self.document.get_element_by_id("launcher-submit").unwrap();
-        launcher_submit.set_attribute("href", dotenv!("SUBMIT_URL"))?;
+        launcher_submit.set_attribute("href", &env::var("SUBMIT_URL").unwrap())?;
 
         // Get Docs Icon
         let launcher_docs = self.document.get_element_by_id("launcher-docs").unwrap();
-        launcher_docs.set_attribute("href", dotenv!("DOCS_URL"))?;
+        launcher_docs.set_attribute("href", &env::var("DOCS_URL").unwrap())?;
 
         // Get Update Icon
         let launcher_update = self.document.get_element_by_id("launcher-update").unwrap();
-        launcher_update.set_attribute("href", dotenv!("UPDATE_URL"))?;
+        launcher_update.set_attribute("href", &env::var("UPDATE_URL").unwrap())?;
 
         // Get Update Icon
         let launcher_discord = self.document.get_element_by_id("launcher-discord").unwrap();
-        launcher_discord.set_attribute("href", dotenv!("DISCORD_URL"))?;
+        launcher_discord.set_attribute("href", &env::var("DISCORD_URL").unwrap())?;
 
         Ok(())
     }
